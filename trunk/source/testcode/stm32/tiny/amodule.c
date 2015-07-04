ac1048c008300b86e761d4e9cca4d6682d2a54ad
fans-rt <fans-rt@f070ef3c-5ae1-46c7-91cf-fc414ebc5686>
2015-07-04 04:24:44 +0000

    【新需求】 添加 TINY BOARD： 堆栈模型： 系统全局内核栈           无 普通任务局部内核栈       有 普通任务局部用户栈       无 内核任务局部内核栈       有 硬件任务切换中断         无

---
+++ trunk/source/testcode/stm32/tiny/amodule.c
@@ -0,0 +1,21 @@
+#include <fauser.h>
+#include <fadefs.h>
+#include <faerror.h>
+#include <famodule.h>
+
+DECLARE_MODULE(Application);
+
+STATIC CONST RO_DATA MODULE_HEADER * g_SystemModuleArray[] =
+{
+    EXCERPT_MODULE(Application),
+};
+
+EXPORT RO_CODE LPMODULE_HEADER CONST * GetModuleArray(VOID)
+{
+    return (LPMODULE_HEADER CONST * ) g_SystemModuleArray;
+}
+
+EXPORT RO_CODE DWORD GetNumberOfSystemModules(VOID)
+{
+    return SIZEOF_ARRAY(g_SystemModuleArray);
+}
