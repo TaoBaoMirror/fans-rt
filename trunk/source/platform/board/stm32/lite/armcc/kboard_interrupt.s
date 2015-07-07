46419cd5f31ee8eeaeff3111dea4b16616b50580
fans-rt <fans-rt@f070ef3c-5ae1-46c7-91cf-fc414ebc5686>
2015-07-07 18:17:22 +0000

    【新需求】 修改HUGE的堆栈切换方式

--- trunk/source/platform/board/stm32/lite/armcc/kboard_interrupt.s
+++ trunk/source/platform/board/stm32/lite/armcc/kboard_interrupt.s
@@ -8,11 +8,11 @@
 ;    If you need for commercial purposes, you should get the author's permission.
 ;
 ;    Configuration:
-;     System global core stack                  NO
-;     The local core stack of general task      YES
+;     System global core stack                  YES
+;     The local core stack of general task      NO
 ;     The loacl core stack of kernel task       YES
 ;     The local user stack of general task      YES
-;     Hardware supported task switch IRQ        NO
+;     Hardware supported task switch IRQ        YES
 ;     Hardware supported double stack           YES
 ;
 ;    date           author          notes
@@ -36,10 +36,8 @@
     IMPORT  CORE_TaskScheduling
     IMPORT  CORE_HandlerLPC
     IMPORT  CORE_SwitchTask
-    IMPORT  CORE_SetTaskStackPosition
-    IMPORT  CORE_GetTaskStackPosition
     IMPORT  CORE_GetCoreStackPosition
-    IMPORT  CORE_CheckMustbeSchedule
+    IMPORT  CORE_GetTaskStackPosition
     IMPORT  CORE_GetCurrentPermission
 
     PRESERVE8
@@ -62,54 +60,24 @@ CORE_Switch2UserMode   PROC
     POP     {PC}                        ;  return
     ENDP
 
-
 PendSV_Handler  PROC
-    BX      LR                          ;  Not support PendSV IRQ
-    ENDP
-
-SVC_Handler     PROC
     CPSID   I                           ;  Why to disable IRQ ? Guess !
-    PUSH    {R4, LR}                    ;  Why to push R4? Guess !
-    PUSH    {R0 - R3}                   ;  Save the parameter of service
-    BL      CORE_EnterIRQ               ;  Set current interrupt nest layer
-    POP     {R0 - R3}                   ;  Reload the parameter of service
-    CPSIE   I                           ;
-    BL      CORE_HandlerLPC             ;  Call system service
-    CPSID   I                           ;
-    POP     {R4}                        ;  Stack must be aligned to 64 bit, so push R4
-    B       ST_L0                       ;  The next step same as systick
-    ENDP
-
-SysTick_Handler PROC
-    CPSID   I                           ;  Why to disable IRQ ? Guess !
-    PUSH    {R4, LR}                    ;  Why to push R0?
-    BL      CORE_EnterIRQ               ;  Set current interrupt nest layer
-    BL      CORE_TickHandler            ;  Inc the system tick
-    CPSIE   I                           ;
-    BL      CORE_TaskScheduling         ;  Find the new task will be scheduling
-    CPSID   I                           ;
-    POP     {R4}                        ;  Stack must be aligned to 64 bit, so push R0
-ST_L0
-    BL      CORE_LeaveIRQ               ;  Set and get current interrupt nest layer
-    CBNZ    R0,     ST_LE               ;  Nest layer != 0 then leave this interrupt
-    BL      CORE_CheckMustbeSchedule    ;  Check need schedule
-    CBZ     R0,     ST_LE               ;  Must schedule = FALSE then leave this interrupt
+    PUSH    {LR}
     BL      CORE_GetCurrentPermission   ;  Check the permission of current task
-    MOV     R2,     R0                  ;  R3 = Permission
+    MOV     R3,     R0                  ;  R3 = Permission
     POP     {LR}
     MRS     R0,     MSP                 ;  R0 = Core stack for old task
     MRS     R1,     PSP                 ;  R1 = User stack for old task
-    CBNZ    R2,     ST_L1               ;  if Permission != CORE then goto ST_L1
+    CBNZ    R3,     PV_L1               ;  R3 = 0  Current task is core task
     STMFD	R0!,    {LR, R4 - R12}      ;  Save core stack for the old task
-    MSR     MSP,    R0                  ;  Update core stack
-    B       ST_L2
-ST_L1
+    MSR     MSP,    R0                  ;  Adjust core stack
+    B       PV_L2
+PV_L1
     STMFD	R1!,    {LR, R4 - R12}      ;  Save user stack for the old task
-    MSR     PSP,    R1                  ;  Update user stack
-ST_L2
+PV_L2    
     BL      CORE_SwitchTask             ;  CORE_SwitchTask(CoreStack, UserStack);
-    MOV     R4,     R0                  ;  R4 = The permission of the new task
-    BL      CORE_GetTaskStackPosition   ;
+    MOV     R4,     R0                  ;  R4 = Permission
+    BL      CORE_GetTaskStackPosition
     MOV     R1,     R0                  ;  R1 = User stack for new task
     BL      CORE_GetCoreStackPosition   ;  R0 = Core stack for new task
     CBNZ    R4,     PV_L3               ;  R4 = 0  Current task is core task
@@ -120,12 +88,41 @@ PV_L3
 PV_L4
     MSR     PSP,    R1                  ;  Update user stack
     MSR     MSP,    R0                  ;  Update core stack
-    PUSH    {LR}                        ;  Because POP PC to return
-ST_LE                                   ;
-    CPSIE   I                           ;
-    POP     {PC}                        ;  Return to task break point
+    CPSIE   I
+    BX      LR
     ENDP
 
+SVC_Handler     PROC
+    CPSID   I
+    PUSH    {LR, R4}                    ;  Why to push R4 ? Guess !
+    PUSH    {R0, R1, R2, R3}            ;
+    BL      CORE_EnterIRQ               ;  Enter Interrupt Critiacl
+    POP     {R0, R1, R2, R3}
+    CPSIE   I
+
+    BL      CORE_HandlerLPC             ;  Call system service
+
+    CPSID   I
+    BL      CORE_LeaveIRQ               ;  Leave Interrupt Critiacl
+    POP     {LR, R4}                    ;  Pop registers
+    CPSIE   I
+    BX      LR
+    ENDP
+    
+SysTick_Handler PROC
+    CPSID   I
+    PUSH    {LR, R4}                    ;  The LR must be push, but the 
+                                        ;  stack is not aligned to 64 bit
+    BL      CORE_EnterIRQ               ;  Enter Interrupt Critiacl
+    BL      CORE_TickHandler            ;  Inc the system tick
+    CPSIE   I
+    BL      CORE_TaskScheduling         ;  Find the new task will be scheduling
+    CPSID   I
+    BL      CORE_LeaveIRQ               ;  Leave Interrupt Critiacl
+    POP     {LR, R4}                    ;  Pop registers
+    CPSIE   I
+    BX      LR
+    ENDP
 
 HardFault_Handler   PROC
     B       .
